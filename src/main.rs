4a54ab0504f5d3a93ca65f5841191ea732d9a0dd
MustacheNinja23 <michaelduffielindsay@gmail.com>
2025-07-27 13:31:39 -0400

    accept connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection");
+                
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
